680bf701d995b560e98a671daeeacd1b59da5a12
Benjamin Fitzpatrick <Benjamin_Fitzpatrick@allfleetservices.co.uk>
2019-03-14 14:37:56 +0000

    first commit

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton1 = Singleton.Instance();
+            Singleton singleton2 = Singleton.Instance();
+            if (singleton1 == singleton2)
+            {
+                Console.WriteLine("Both instances of the singleton are the same");
+            }
+            Console.ReadLine();
+        }
+    }
+}
